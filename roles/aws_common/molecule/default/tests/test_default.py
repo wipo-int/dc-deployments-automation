bc893a0a2e20909c5a2c510a9af2100250c75a34
Steve Smith <ssmith@atlassian.com>
2019-04-11 11:23:15 +1000

    DCD-224: Ensure ec2-utils is installed and add testing.

---
+++ roles/aws_common/molecule/default/tests/test_default.py
@@ -0,0 +1,16 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize('exe', [
+    '/usr/bin/ec2-metadata',
+    '/usr/bin/amazon-ssm-agent',
+    '/sbin/mount.efs'
+])
+def test_package_exes(host, exe):
+    assert host.file(exe).exists
