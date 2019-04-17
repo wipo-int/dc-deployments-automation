87c5b0ba817e38813bb52b204aba3b8995a162aa
Steve Smith <ssmith@atlassian.com>
2019-04-17 14:21:17 +1000

    DCD-224: Inject JVM heap overrides into the product setenv script.

--- roles/jira_config/molecule/default/tests/test_default.py
+++ roles/jira_config/molecule/default/tests/test_default.py
@@ -6,10 +6,16 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
+def test_dbconfig_file(host):
     f = host.file('/var/atlassian/application-data/jira/dbconfig.xml')
     assert f.exists
     assert f.user == 'jira'
     assert f.contains("<driver-class>org.postgresql.Driver</driver-class>")
     assert f.contains("<username>atljira</username>")
     assert f.contains("<pool-min-size>20</pool-min-size>")
+
+def test_setenv_file(host):
+    f = host.file('/opt/atlassian/jira-software/current/bin/setenv.sh')
+    assert f.exists
+    assert f.contains('JVM_MINIMUM_MEMORY="PLACEHOLDER"')
+    assert f.contains('JVM_MAXIMUM_MEMORY="PLACEHOLDER"')
