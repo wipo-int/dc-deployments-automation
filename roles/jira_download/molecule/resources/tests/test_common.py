86686c08ef908db714f042e4ae0ec590bef6b41f
Steve Smith <ssmith@atlassian.com>
2019-04-17 14:54:18 +1000

    DCD-224: Fix unpacking of product.

--- roles/jira_download/molecule/resources/tests/test_common.py
+++ roles/jira_download/molecule/resources/tests/test_common.py
@@ -15,3 +15,7 @@ def test_symlink_created(host):
     target = host.file('/opt/atlassian/jira-software/current')
     assert target.exists
     assert target.is_symlink
+
+def test_unpacked(host):
+    verfile = host.file('/opt/atlassian/jira-software/current/bin/catalina.sh')
+    assert verfile.exists
