efc14025ea76459db4173896afb8ccf7289266c1
Steve Smith <ssmith@atlassian.com>
2019-04-30 11:21:28 +1000

    DCD-224: Add lookup table for appropriate ServiceDesk version depending on the Jira major version.

--- roles/jira_config/molecule/servicedesk_with_latest/tests/test_default.py
+++ roles/jira_config/molecule/servicedesk_with_latest/tests/test_default.py
@@ -24,3 +24,14 @@ def test_is_unpacked(host):
     assert installer.is_directory
     assert installer.user == 'jira'
     assert installer.mode == 0o0755
+
+def test_sd_is_downloaded(host):
+    installer = host.file('/opt/atlassian/tmp/jira-servicedesk-4.1.0.obr')
+    assert installer.exists
+    assert installer.user == 'root'
+
+def test_is_unpacked(host):
+    installer = host.file('/media/atl/jira/shared/plugins/installed-plugins/jira-servicedesk-application-4.1.0.jar')
+    assert installer.exists
+    assert installer.user == 'jira'
+    assert installer.mode == 0o0750
