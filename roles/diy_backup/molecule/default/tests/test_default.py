ee945a3ca0f0e94299cba6841b8ecb2de334bb1b
Steve Smith <ssmith@atlassian.com>
2019-05-31 13:09:47 +1000

    DCD-386: Start of DIY backup installation.

---
+++ roles/diy_backup/molecule/default/tests/test_default.py
@@ -0,0 +1,12 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_git_clone(host):
+    f = host.file('/opt/atlassian/bitbucket-diy-backup')
+    assert f.exists
+    assert f.is_directory
