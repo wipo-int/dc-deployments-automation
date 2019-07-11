4dff688b3d3d7c9d22af1748afe493e9e1387482
Steve Smith <ssmith@atlassian.com>
2019-07-11 13:57:38 +1000

    DCD-484: Rename use_system_jdk to match the other vars.

---
+++ roles/product_common/molecule/system_jdk/tests/test_default.py
@@ -0,0 +1,20 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_user_created(host):
+    user = host.user('testuser')
+    assert user.exists
+
+
+@pytest.mark.parametrize('exe', [
+    '/usr/bin/git',
+    '/usr/bin/javac'
+])
+def test_package_exes(host, exe):
+    assert host.file(exe).exists
