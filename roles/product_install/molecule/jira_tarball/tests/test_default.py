8022b591573967fcb1e2773757f8d1d466504210
Glenn Stewart <gstewart@atlassian.com>
2019-11-01 12:06:08 +1100

    ITOPS-2158 testing

--- roles/product_install/molecule/jira_tarball/tests/test_default.py
+++ roles/product_install/molecule/jira_tarball/tests/test_default.py
@@ -23,6 +23,6 @@ def test_latest_is_downloaded(host):
     upstream_json = json.load(upstream_fd)
     upstream = upstream_json['version']
 
-    installer = host.file('/opt/atlassian/tmp/jira-core.'+upstream+'.tar.gz')
+    installer = host.file('/media/atl/jira/shared/downloads/jira-core.'+upstream+'.tar.gz')
     assert installer.exists
     assert installer.user == 'root'
