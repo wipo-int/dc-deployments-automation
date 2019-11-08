1cf02e721fabfa5154b2dbff9582003236ff3941
Brett Meehan <bmeehan@atlassian.com>
2019-11-08 15:59:48 +1100

    ITOPSENG-101 moved core tests to software

--- roles/product_install/molecule/jira_all/tests/test_default.py
+++ roles/product_install/molecule/jira_all/tests/test_default.py
@@ -9,23 +9,23 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_version_is_correct(host):
-    verfile = host.file('/media/atl/jira/shared/jira-core.version')
+    verfile = host.file('/media/atl/jira/shared/jira-software.version')
     assert verfile.exists
 
     assert verfile.content.decode("UTF-8").strip() == "7.13.1"
 
 def test_is_downloaded(host):
-    installer = host.file('/media/atl/downloads/jira-core.7.13.1-x64.bin')
+    installer = host.file('/media/atl/downloads/jira-software.7.13.1-x64.bin')
     assert installer.exists
     assert installer.user == 'root'
 
 def test_completed_lockfile(host):
-    lockfile = host.file('/media/atl/downloads/jira-core.7.13.1-x64.bin_completed')
+    lockfile = host.file('/media/atl/downloads/jira-software.7.13.1-x64.bin_completed')
     assert lockfile.exists
     assert lockfile.user == 'root'
 
 def test_is_unpacked(host):
-    installer = host.file('/opt/atlassian/jira-core/7.13.1/atlassian-jira/')
+    installer = host.file('/opt/atlassian/jira-software/7.13.1/atlassian-jira/')
     assert installer.exists
     assert installer.is_directory
     assert installer.user == 'jira'
