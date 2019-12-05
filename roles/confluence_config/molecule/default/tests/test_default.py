8c02ea36bbc0a2640197107eb6f35f06b6600c9c
Brett Meehan <bmeehan@atlassian.com>
2019-12-05 15:12:33 +1100

    ITOPSENG-277 added fixes for tests as per discussion with Ben

--- roles/confluence_config/molecule/default/tests/test_default.py
+++ roles/confluence_config/molecule/default/tests/test_default.py
@@ -38,8 +38,6 @@ def test_server_file(host):
     assert f.contains('acceptCount="10"')
     assert f.contains('secure="false"')
     assert f.contains('scheme="http"')
-    assert not f.contains('proxyName=')
-    assert not f.contains('proxyPort=')
 
 def test_install_permissions(host):
     assert host.file('/opt/atlassian/confluence/current/conf/server.xml').user == 'root'
