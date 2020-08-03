09c0a493414dc4cdc94a0408738c0a7202239d6f
Adam Brokes <abrokes@atlassian.com>
2020-08-03 15:25:30 +1000

    DCD-963: Add test for preferred test query

--- roles/confluence_config/molecule/default/tests/test_default.py
+++ roles/confluence_config/molecule/default/tests/test_default.py
@@ -78,3 +78,4 @@ def test_confluence_config_file(host):
     assert f.contains('<property name="confluence.cluster.aws.host.header">ec2.amazonaws.com</property>')
     assert f.contains('<property name="hibernate.connection.url">jdbc:postgresql://postgres-db.ap-southeast-2.rds.amazonaws.com:5432/confluence</property>')
     assert f.contains('<property name="hibernate.connection.password">molecule_password</property>')
+    assert f.contains('<property name="hibernate.c3p0.preferredTestQuery">select 1;</property>')
