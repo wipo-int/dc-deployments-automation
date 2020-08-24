db84840a96016b5cf3e10edf294da21bcb01d555
Steve Smith <ssmith@atlassian.com>
2020-08-24 13:23:17 +1000

    DCD-1020: Add test for font installation with system JDK.

---
+++ roles/confluence_config/molecule/system_jdk/tests/test_default.py
@@ -0,0 +1,20 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+def test_seraph_file(host):
+    f = host.file('/opt/atlassian/confluence/current/confluence/WEB-INF/classes/seraph-config.xml')
+    assert f.exists
+    assert f.contains('<param-value>COOKIEAGE</param-value>')
+
+@pytest.mark.parametrize('font', [
+    '/usr/lib/jvm/java/lib/fonts/fallback/NotoSansJavanese-Regular.ttf',
+    '/usr/lib/jvm/jre/lib/fonts/fallback/NotoSansJavanese-Regular.ttf'
+])
+def test_fonts_installed_and_linked(host, font):
+    f = host.file(font)
+    assert f.exists
