f3e3853cbdd27f6384b54b892d8923f2ef327b0d
Denise Unterwurzacher <dunterwurzacher@atlassian.com>
2021-01-04 16:57:42 -0600

    ITPLAT-387 catalina.err isn't a standard log file - standard Tomcat logging directs both stdout and stderr to catalina.out. See https://tomcat.apache.org/tomcat-8.0-doc/logging.html

--- roles/product_startup/molecule/bitbucket/tests/test_default.py
+++ roles/product_startup/molecule/bitbucket/tests/test_default.py
@@ -9,7 +9,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_service_file(host):
     systemd_prefix = "/lib" if host.system_info.distribution == "ubuntu" else "/usr/lib"
     f = host.file(systemd_prefix+'/systemd/system/bitbucket.service')
-    assert f.contains("^ExecStart=/opt/atlassian/bitbucket/current/bin/start-bitbucket.sh -fg --no-search >/opt/atlassian/bitbucket/current/logs/catalina.out 2>/opt/atlassian/bitbucket/current/logs/catalina.err$")
+    assert f.contains("^ExecStart=/opt/atlassian/bitbucket/current/bin/start-bitbucket.sh -fg --no-search >/opt/atlassian/bitbucket/current/logs/catalina.out 2>&1$")
     assert f.contains("^UMask=0027$")
     assert f.contains("^LimitNOFILE=4096$")
     assert f.contains("^Environment=BITBUCKET_HOME=/media/atl/bitbucket$")
