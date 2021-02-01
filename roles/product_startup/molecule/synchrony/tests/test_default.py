d3e6ba137c2ba44736e7f140e5df2c6c3eea8840
Brett Meehan <bmeehan@atlassian.com>
2021-02-02 10:14:22 +1100

    ITPLAT-387 exclude bitbucket from catalina.out fix

--- roles/product_startup/molecule/synchrony/tests/test_default.py
+++ roles/product_startup/molecule/synchrony/tests/test_default.py
@@ -9,7 +9,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_service_file(host):
     systemd_prefix = "/lib" if host.system_info.distribution == "ubuntu" else "/usr/lib"
     f = host.file(systemd_prefix+'/systemd/system/synchrony.service')
-    assert f.contains("^ExecStart=/opt/atlassian/bin/start-synchrony >/opt/atlassian/confluence/current/logs/catalina.out 2>/opt/atlassian/confluence/current/logs/catalina.err$")
+    assert f.contains("^ExecStart=/opt/atlassian/bin/start-synchrony >/opt/atlassian/confluence/current/logs/catalina.out 2>&1$")
     assert f.contains("^EnvironmentFile=/etc/atl$")
     assert f.contains("^EnvironmentFile=/etc/atl.synchrony$")
     assert f.contains("^WorkingDirectory=/opt/atlassian/confluence/current/logs/$")
