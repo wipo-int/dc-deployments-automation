4bacc33c5a9831f81f2c7f1514156399ca309269
Adam Brokes <abrokes@atlassian.com>
2021-02-12 13:44:46 +1100

    DCD-1216: Change the url to fetch Jira buildNumber for OBR compatability

--- roles/product_install/molecule/jira_all/tests/test_default.py
+++ roles/product_install/molecule/jira_all/tests/test_default.py
@@ -12,37 +12,37 @@ def test_version_is_correct(host):
     verfile = host.file('/media/atl/jira/shared/jira-software.version')
     assert verfile.exists
 
-    assert verfile.content.decode("UTF-8").strip() == "7.13.1"
+    assert verfile.content.decode("UTF-8").strip() == "8.14.0"
 
 def test_is_downloaded(host):
-    installer = host.file('/media/atl/downloads/jira-software.7.13.1-x64.bin')
+    installer = host.file('/media/atl/downloads/jira-software.8.14.0-x64.bin')
     assert installer.exists
     assert installer.user == 'root'
 
 def test_completed_lockfile(host):
-    lockfile = host.file('/media/atl/downloads/jira-software.7.13.1-x64.bin_completed')
+    lockfile = host.file('/media/atl/downloads/jira-software.8.14.0-x64.bin_completed')
     assert lockfile.exists
     assert lockfile.user == 'root'
 
 def test_is_unpacked(host):
-    installer = host.file('/opt/atlassian/jira-software/7.13.1/atlassian-jira/')
+    installer = host.file('/opt/atlassian/jira-software/8.14.0/atlassian-jira/')
     assert installer.exists
     assert installer.is_directory
     assert installer.user == 'jira'
     assert installer.mode == 0o0755
 
 def test_obr_is_downloaded(host):
-    installer = host.file('/media/atl/downloads/jira-servicedesk-application-3.16.1.obr')
+    installer = host.file('/media/atl/downloads/jira-servicedesk-application-4.14.0.obr')
     assert installer.exists
     assert installer.user == 'root'
 
 def test_obr_completed_lockfile(host):
-    lockfile = host.file('/media/atl/downloads/jira-servicedesk-application-3.16.1.obr_completed')
+    lockfile = host.file('/media/atl/downloads/jira-servicedesk-application-4.14.0.obr_completed')
     assert lockfile.exists
     assert lockfile.user == 'root'
 
 def test_obr_is_unpacked(host):
-    jsdjar = host.file('/media/atl/jira/shared/plugins/installed-plugins/jira-servicedesk-application-3.16.1.jar')
+    jsdjar = host.file('/media/atl/jira/shared/plugins/installed-plugins/jira-servicedesk-application-4.14.0.jar')
     assert jsdjar.exists
     assert jsdjar.user == 'jira'
     assert jsdjar.mode == 0o0750
