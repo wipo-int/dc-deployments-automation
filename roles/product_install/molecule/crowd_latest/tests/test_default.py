60ce28df7812e9964eec30fc01d97628bfa2df2a
Yevhen Karnaukhov <ykarnaukhov@atlassian.com>
2022-05-13 14:04:46 +0200

    DCD-1526 Update Ansible playbook "latest" logic for Bitbucket, Confluence, and Crowd

--- roles/product_install/molecule/crowd_latest/tests/test_default.py
+++ roles/product_install/molecule/crowd_latest/tests/test_default.py
@@ -24,28 +24,25 @@ def test_version_file_is_latest(host):
     verfile = host.file('/media/atl/crowd/shared/crowd.version')
     assert verfile.exists
 
-    upstream_fd = urllib.request.urlopen(
-        "https://marketplace.atlassian.com/rest/2/applications/crowd/versions/latest")
+    upstream_fd = urllib.request.urlopen("https://marketplace.atlassian.com/rest/2/products/key/crowd/versions")
     upstream_json = json.load(upstream_fd)
-    upstream = upstream_json['version']
+    upstream = upstream_json['_embedded']['versions'][0]['name']
 
     assert verfile.content.decode("UTF-8").strip() == upstream.strip()
 
 def test_latest_is_downloaded(host):
-    upstream_fd = urllib.request.urlopen(
-        "https://marketplace.atlassian.com/rest/2/applications/crowd/versions/latest")
+    upstream_fd = urllib.request.urlopen("https://marketplace.atlassian.com/rest/2/products/key/crowd/versions")
     upstream_json = json.load(upstream_fd)
-    upstream = upstream_json['version']
+    upstream = upstream_json['_embedded']['versions'][0]['name']
 
     installer = host.file('/media/atl/downloads/crowd.' + upstream + '.tar.gz')
     assert installer.exists
     assert installer.user == 'root'
 
 def test_completed_lockfile(host):
-    upstream_fd = urllib.request.urlopen(
-        "https://marketplace.atlassian.com/rest/2/applications/crowd/versions/latest")
+    upstream_fd = urllib.request.urlopen("https://marketplace.atlassian.com/rest/2/products/key/crowd/versions")
     upstream_json = json.load(upstream_fd)
-    upstream = upstream_json['version']
+    upstream = upstream_json['_embedded']['versions'][0]['name']
 
     lockfile = host.file('/media/atl/downloads/crowd.' + upstream + '.tar.gz_completed')
     assert lockfile.exists
