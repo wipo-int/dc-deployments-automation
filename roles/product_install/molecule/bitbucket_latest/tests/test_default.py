0f08266af5bcd887988b2e9d165f5f11e7d5bb21
nghazalibeiklar <nghazalibeiklar@atlassian.com>
2022-06-23 17:16:41 +1000

    added nosec comment

--- roles/product_install/molecule/bitbucket_latest/tests/test_default.py
+++ roles/product_install/molecule/bitbucket_latest/tests/test_default.py
@@ -45,7 +45,7 @@ def test_completed_lockfile(host):
 def get_version(url):
     assert url.lower().startswith('http')
     upstream_req = urllib.request.Request(url)
-    with urllib.request.urlopen(upstream_req) as upstream_response:
+    with urllib.request.urlopen(upstream_req) as upstream_response: #nosec
         upstream_json = json.load(upstream_response)
         upstream = upstream_json['_embedded']['versions'][0]['name']
     return upstream
